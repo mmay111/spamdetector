72ef336455b7240593f7901109dede8068b798c4
muhammed.ay <muhammed.ay@katu.com.tr>
2022-04-24 16:55:17 +0300

    new comment

--- spamapp/mails/views.py
+++ spamapp/mails/views.py
@@ -4,6 +4,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from mails.models import mails
 # Create your views here.
+#created views
 
 
 #all, spam/ not spam ssyfalrını da ekle
@@ -11,7 +12,7 @@ def index(request):
     context = {
         "mails": mails.objects.all(),
         #"mails": mails.objects.filter(is_active=True),
-        "xx": "Duyurular" 
+        "xx": "Duyurular"  
     }
     
     return render(request, "index.html",context)
